50528755add4b2d8e4588d4116ed23f09054a556
leonking17 <leonjevans@yahoo.com>
2021-07-05 13:55:47 -0500

    adding some loops into the code for additional features

--- coffee_bot.py
+++ coffee_bot.py
@@ -27,18 +27,29 @@ def order_latte():
     print_message()
     return order_latte()
 
+def order_mocha():
+  while True:
+   res = input('Would you like to try our limited-edition peppermint mocha? \n[a] Sure! \n[b] Maybe next time! \n> ')
+
+   if res == 'a':
+     return 'peppermint mocha'
+   elif res == 'b':
+     return 'mocha'
+   print_message()
+
 
 def get_drink_type():
- res = input('What type of drink would you like? \n[a] Brewed Coffee \n[b] Mocha \n[c] Latte \n> ')
- if res == 'a':
-   return 'brewed coffee'
- elif res == 'b':
-  return 'mocha'
- elif res == 'c':
-  return order_latte()
- else:
-  print_message()
-  return get_drink_type()
+  res = input('What type of drink would you like? \n[a] Brewed Coffee \n[b] Mocha \n[c] Latte \n> ')
+
+  if res == 'a':
+    return 'brewed coffee'
+  elif res == 'b':
+    return order_mocha()
+  elif res == 'c':
+    return order_latte()
+  else:
+    print_message()
+    return get_drink_type()
 
 
 def hot_or_cold():
@@ -54,15 +65,31 @@ def hot_or_cold():
 
 def coffee_bot():
   print('Welcome to the cafe!')
-  size = get_size()
-  drink_type = get_drink_type()
-  hot_cold = hot_or_cold()
-  if hot_cold == 'Yes':
-    print('Alright, that\'s a {} iced {}!'.format(size, drink_type))
-  else:
-     print('Alright, that\'s a {} {}!'.format(size, drink_type))
-  name = input('Can I get your name please? ')
-  print('Thanks, {}! Your drink will be ready shortly.'.format(name))
+
+  order_drink = 'y'
+  drinks = []
+
+  while order_drink == 'y':
+    size = get_size()  
+    drink_type = get_drink_type()
+
+    drink = '{} {}'.format(size, drink_type)
+    print('Alright, that\'s a {}!'.format(drink))
+    drinks.append(drink)
+
+    while True:
+      order_drink = input('Would you like to order anouther drink? (y/n) \n> ')
+
+      if order_drink in ['y', 'n']:
+        break
+    print('Okay so I have: ')
+
+    for drink in drinks:
+      print('-', drink)
+
+  
+  name = input('Can I get your name please? \n> ')
+  print('Thanks, {}! Your order will be ready shortly.'.format(name))
 
 # Call coffee_bot()!
 coffee_bot()
